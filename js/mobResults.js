7b5ac9291d1023ea182b9ed482e68c45d3a92388
Cody Smith <cms287@gmail.com>
2018-09-10 20:34:16 -0700

    80%

--- js/mobResults.js
+++ js/mobResults.js
@@ -3,10 +3,23 @@
 const mobResults = document.querySelector("#mobResults");
 const footTable = document.querySelector("#footTable");
 const kneeTable = document.querySelector("#kneeTable");
-const lphcTable = document.querySelector("#lphcTable");
+const forwardLeanTable = document.querySelector("#forwardLeanTable");
+const weightShiftTable = document.querySelector("#weightShiftTable");
+const lowBackTable = document.querySelector("#lowbackTable");
 const shoulderTable = document.querySelector("#shoulderTable");
 
-// footTable Compensations
+// Dynamic td's
+let weightShiftPhase1 = document.querySelector("#weightShiftPhase1");
+let weightShiftPhase2 = document.querySelector("#weightShiftPhase2");
+let weightShiftPhase3 = document.querySelector("#weightShiftPhase3");
+let weightShiftPhase4 = document.querySelector("#weightShiftPhase4");
+
+let weightShiftPhase1 = document.querySelector("#lowbackPhase1");
+let weightShiftPhase2 = document.querySelector("#lowbackPhase2");
+let weightShiftPhase3 = document.querySelector("#lowbackPhase3");
+let weightShiftPhase4 = document.querySelector("#lowbackPhase4");
+
+// footTable Compensations - localStorage
 let feetTurnOut = (localStorage.getItem('feetTurnOut'));
 let feetFlatten1 = (localStorage.getItem('feetFlatten1'));
 let feetFlatten2 = (localStorage.getItem('feetFlatten2'));
@@ -23,11 +36,13 @@ let forwardLean = (localStorage.getItem('forwardLean'));
 let weightShiftLeft = (localStorage.getItem('weightShiftLeft'));
 let weightShiftRight = (localStorage.getItem('weightShiftRight'));
 
+
 // shoulderTable Compensations
 let armsFallForward = (localStorage.getItem('armsFallForward'));
 
 
 window.addEventListener("load", e=> {
+  // Foot Compensations
   function footTableComps() {
     if (feetTurnOut === "checked" || feetFlatten1 === "checked" || feetFlatten2 === "checked" || heelsRise === "checked") {
       footTable.classList.remove("ghost");
@@ -35,6 +50,7 @@ window.addEventListener("load", e=> {
   };
   footTableComps();
 
+  // Knee Compensations
   function kneeTableComps() {
     if (kneesMoveIn === "checked" || kneesMoveOut === "checked") {
       kneeTable.classList.remove("ghost");
@@ -42,12 +58,20 @@ window.addEventListener("load", e=> {
   };
   kneeTableComps();
 
+  // Excessive Forward Lean Compensation
+  function forwardLeanComp() {
+    if (forwardLean === "checked") {
+      forwardLeanTable.classList.remove("ghost");
+    } else {}
+  };
+  forwardLeanComp();
+
+  // Arms Fall Forward Compensation
   function shoulderTableComp() {
     if (armsFallForward === "checked") {
       shoulderTable.classList.remove("ghost");
     } else {}
   };
   shoulderTableComp();
-
 
 });
