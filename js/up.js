dacf824be0e1ca4cd3a22a030f51e00f4844368e
Abby Suh <abbysuh@hotmail.com>
2018-09-12 20:47:10 -0700

    HTML changes, add some JS

---
+++ js/up.js
@@ -0,0 +1,25 @@
+'use strict'
+const form = document.forms.userProfile;
+const ageInput = document.querySelector('#age').value;
+const heightInput = document.querySelector('#height').value;
+const weightInput = document.querySelector('#weight').value;
+const bmiInput = document.querySelector('#bmi').value;
+const cityInput = document.querySelector('#city').value;
+const stateInput = document.querySelector('#state').value;
+const fitnessGoalsInput = document.querySelector('#fitnessGoals').value;
+
+console.log(ageInput);
+
+// function populateLocalStorage() {
+//     localStorage.setItem('age', 'ageInput');
+//     localStorage.setItem('height', 'heightInput');
+//     localStorage.setItem('weight', 'weightInput');
+//     localStorage.setItem('bmi', 'bmiInput');
+//     localStorage.setItem('city', 'cityInput');
+//     localStorage.setItem('state', 'stateInput');
+//     localStorage.setItem('fitnessGoals', 'fitnessGoalsInput');
+// };
+// populateLocalStorage();
+
+console.log(ageInput);
+
